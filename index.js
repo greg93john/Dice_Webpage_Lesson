9c12090c670f922167d67a7454bc7b2124f00afe
Greg Johnson <greg93John@gmail.com>
2020-11-19 12:55:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var pNum1 = Math.floor(1 + (Math.random() * 6));
-document.querySelector(".img1").setAttribute("src", "images/dice"+pNum1+".png");
-var pNum2 = Math.floor(1 + (Math.random() * 6));
-document.querySelector(".img2").setAttribute("src", "images/dice"+pNum2+".png");
-if(pNum1 !== pNum2) {
-  document.querySelector("h1").innerText = "Player " + (pNum1 > pNum2 ? "1" : "2") + " Wins!";
-} else{
-  document.querySelector("h1").innerText = "It's a Draw!";
-}
-
 document.querySelector(".reroll").addEventListener("click", reroll);
 
 function reroll() {
